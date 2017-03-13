b69782e3159edf106ffd0440f19ce6b245cf3ecd
Jstryd <jstryd@vcd-ch204-07.eastern.ewu.edu>
2017-03-13 14:11:35 -0700

    added simple weather plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 // YOUR SCRIPTS GO HERE
+
+// simple weather from unheap
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+
+$.simpleWeather({
+  location: 99004,
+  unit: 'f',
+  
+  success: function(weather){
+    //check for weather
+    console.log(weather);
+    
+    // display weather
+    $('.temp').text(weather.temp);
+    
+    // display city
+    $('.city').text(weather.city);
+    
+    // display state
+    $('.state').text(weather.region);
+    
+    $('.image img').attr('src', weather.image);
+  },
+
+  
+  error: function(error){
+    console.log('Refresh page.');
+  }
+});
